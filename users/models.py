aea2ea8ff443055b5d52e8a0aed1cdebdaf33352
seungminyi <seungminyi95@gmail.com>
2020-04-02 22:16:49 +0900

    second commit

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+
+    """  Custom User Model """
+
+    avatar = models.ImageField(null=True)
+    gender = models.CharField(max_length=10, null=True)
+    bio = models.TextField(default="")
