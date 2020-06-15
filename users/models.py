f27277b438edfa48cbdab01645d0ca4ec3659f18
seungminyi <seungminyi95@gmail.com>
2020-06-15 22:53:15 +0900

    many to many Field

--- users/models.py
+++ users/models.py
@@ -6,6 +6,35 @@ class User(AbstractUser):
 
     """  Custom User Model """
 
-    avatar = models.ImageField(null=True)
-    gender = models.CharField(max_length=10, null=True)
-    bio = models.TextField(default="")
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOISES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(
+        default=GENDER_MALE, choices=GENDER_CHOICES, max_length=10, blank=True,
+    )
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOISES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
