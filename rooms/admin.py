9f651214a970f000ece273232935ceb2735ca8ff
seungminyi <seungminyi95@gmail.com>
2020-06-15 23:55:02 +0900

    mata class

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.RoomType)
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.House_rules)
 class ItemAdmin(admin.ModelAdmin):
     pass
 
--- rooms/models.py
+++ rooms/models.py
@@ -22,6 +22,31 @@ class AbstactItem(core_models.TimeStampedModel):
 class RoomType(AbstactItem):
     pass
 
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["created"]
+
+
+class Amenity(AbstactItem):
+    pass
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstactItem):
+    pass
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class House_rules(AbstactItem):
+    pass
+
+    class Meta:
+        verbose_name = "House Rule"
+
 
 class Room(core_models.TimeStampedModel):
     """ Room Model Definition """
@@ -40,7 +65,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(House_rules, blank=True)
 
     def __str__(self):
         return self.name
