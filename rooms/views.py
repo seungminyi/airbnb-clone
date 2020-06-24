f2697c0741b1fcdc7933a6a5fead7edef09b23c1
seungminyi <seungminyi95@gmail.com>
2020-06-25 07:00:54 +0900

    add rooms template

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from . import models
 
 # Create your views here.
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(page)
+        return render(request, "rooms/home.html", {"page": rooms},)
+    except (EmptyPage, PageNotAnInteger):
+        return redirect("/")
+
