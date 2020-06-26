b875d44fe1ccf647300536fbe0f8d2b29a94e0a7
seungminyi <seungminyi95@gmail.com>
2020-06-26 14:52:45 +0900

    add room pages

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import ObjectDoesNotExist
+from django.http import Http404
+from django.shortcuts import render, redirect, reverse
+from django.views.generic import ListView, DetailView
 from . import models
 
 # Create your views here.
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(page)
-        return render(request, "rooms/home.html", {"page": rooms},)
-    except (EmptyPage, PageNotAnInteger):
-        return redirect("/")
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    ordering = ["pk"]
+
+
+class RoomDetail(DetailView):
+    model = models.Room
 
