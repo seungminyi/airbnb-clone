e4629f7434cf012d58553c3a8d5977582f4a9ff1
seungminyi <seungminyi95@gmail.com>
2020-07-18 21:38:51 +0900

    modify search page

--- rooms/views.py
+++ rooms/views.py
@@ -2,6 +2,7 @@ from django.db.models import ObjectDoesNotExist
 from django.http import Http404
 from django.shortcuts import render, redirect, reverse
 from django.views.generic import ListView, DetailView
+from django_countries import Countries
 from . import models
 
 # Create your views here.
@@ -22,5 +23,13 @@ def search(request):
 
     city = request.GET.get("city", "")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city": city})
+    country = request.GET.get("country")
+    room_type = int(request.GET.get("room_type"))
+    room_types = models.RoomType.objects.all()
+
+    form = {"city": city, "s_room_type": room_type, "s_country": country}
+
+    choices = {"countries": Countries, "room_types": room_types}
+
+    return render(request, "rooms/search.html", {**form, **choices},)
 
